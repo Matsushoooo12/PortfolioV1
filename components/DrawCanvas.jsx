998083a6fcaf04b3a45162b8b029a78d5222a66c
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-09-26 15:48:54 +0900

    ローディングの色修正

--- components/DrawCanvas.jsx
+++ components/DrawCanvas.jsx
@@ -15,7 +15,7 @@ const DrawCanvas = () => (
           thickness="4px"
           speed="0.65s"
           emptyColor="gray.200"
-          color="blue.500"
+          color="teal.500"
           size="xl"
         />
       </Center>
